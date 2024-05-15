2db66430ca6ea3e9f803569f4f4eb088f7f02f3f
paulovarrone <paulorobertovarrone@gmail.com>
2024-05-15 14:03:37 -0300

    creating paths

--- programa/ChatPdf.py
+++ programa/ChatPdf.py
@@ -1,6 +1,10 @@
+import os
+
 def main():
    try:
        caminho_pdf = r'C:\Users\3470622\Desktop\ChatPdfLocal\pdf'
+       caminho_banco = r'C:ChatPdfLocal\base_de_dados'
+       BancoVetor = os.path.exists(caminho_banco)
 
    except Exception as e:
        print(f"Erro na função: main  {caminho_pdf}: {e}")
