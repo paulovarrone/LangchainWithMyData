f5310f90423c951070b33543f02963bfa1b922eb
paulovarrone <paulorobertovarrone@gmail.com>
2024-05-15 12:18:35 -0300

    main

---
+++ programa/ChatPdf.py
@@ -0,0 +1,9 @@
+def main():
+   try:
+       caminho_pdf = 'C:\Users\3470622\Desktop\ChatPdfLocal\pdf'
+
+   except Exception as e:
+       print(f"Erro na função: main  {caminho_pdf}: {e}")
+
+if __name__ is "__main__":
+   main()
