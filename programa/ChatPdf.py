bc963c8cabcd4d487ee64e8c0ffe4710ffe4b158
Paulo Varrone <paulorobertovarrone@gmail.com>
2024-05-15 18:03:00 -0300

    error handling

--- programa/ChatPdf.py
+++ programa/ChatPdf.py
@@ -3,9 +3,9 @@ import os
 def main():
    try:
        caminho_pdf = r'C:\Users\3470622\Desktop\ChatPdfLocal\pdf'
-       caminho_banco = r'C:ChatPdfLocal\base_de_dados'
+       caminho_banco = r'C:\Users\3470622\Desktop\ChatPdfLocal\BancoVetor'
        BancoVetor = os.path.exists(caminho_banco)
-
+       
    except Exception as e:
        print(f"Erro na função: main  {caminho_pdf}: {e}")
 
