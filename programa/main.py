aff61f9c919617df2a741b4dca29481d0260b753
Paulo Varrone <paulorobertovarrone@gmail.com>
2024-05-15 23:24:27 -0300

    main

---
+++ programa/main.py
@@ -0,0 +1,18 @@
+import os
+from vetorizar import pdf_loader_and_splitter
+
+def main():
+   try:
+       caminho_pdf = r'C:\Users\3470622\Desktop\ChatPdfLocal\pdf'
+       caminho_banco = r'C:\Users\3470622\Desktop\ChatPdfLocal\BancoVetor'
+       
+       conteudo_pasta = os.listdir(caminho_pdf)
+       BancoVetor = os.path.exists(caminho_banco)
+
+       pdf_loader_and_splitter(BancoVetor)
+
+   except Exception as e:
+       print(f"Erro na função: main  {e}")
+
+if __name__ == "__main__":
+   main()
